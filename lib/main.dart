b33165be0d5baf189b2f9ba49e174fa2ae42f8f5
gokulasuriyakvp <134067562+gokulasuriyakvp@users.noreply.github.com>
2023-11-29 22:00:59 +0530

    initialcommit

--- lib/main.dart
+++ lib/main.dart
@@ -22,11 +22,12 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-    // home: SafeAreaRef(),
-      // home: TimePickerRef(),
+      // home: SafeAreaRef(),
+       home: TimePickerRef(),
       //home: DatePickerRef(),
-      home: RatingStarBarRef(),
+      //home: RatingStarBarRef(),
     );
   }
 }
+
 
--- lib/time_picker.dart
+++ lib/time_picker.dart
@@ -14,6 +14,7 @@ class _TimePickerRefState extends State<TimePickerRef> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Time Picker Reference'),
+        backgroundColor: Colors.blue,
       ),
       body: Center(
         child: Column(
